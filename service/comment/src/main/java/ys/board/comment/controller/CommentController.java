b654d21fc3f0ac0da5871800106ed5344c1892e5
yssy3135 <ys3135@gmail.com>
2025-02-07 00:16:39 +0900

    commend cud

---
+++ service/comment/src/main/java/ys/board/comment/controller/CommentController.java
@@ -0,0 +1,43 @@
+package ys.board.comment.controller;
+
+import ys.board.comment.service.CommentService;
+import ys.board.comment.service.request.CommentCreateRequest;
+import ys.board.comment.service.response.CommentPageResponse;
+import ys.board.comment.service.response.CommentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/v1/comments/{commentId}")
+    public CommentResponse read(
+            @PathVariable("commentId") Long commentId
+    ) {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v1/comments")
+    public CommentResponse create(@RequestBody CommentCreateRequest request) {
+        return commentService.create(request);
+    }
+
+    @DeleteMapping("/v1/comments/{commentId}")
+    public void delete(@PathVariable("commentId") Long commentId) {
+        commentService.delete(commentId);
+    }
+
+    @GetMapping("/v1/comments")
+    public CommentPageResponse readAll(
+            @RequestParam("articleId") Long articleId,
+            @RequestParam("page") Long page,
+            @RequestParam("pageSize") Long pageSize
+    ) {
+        return commentService.readAll(articleId, page, pageSize);
+    }
+
+}
