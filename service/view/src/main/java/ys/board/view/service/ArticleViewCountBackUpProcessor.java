79ee9dffb0916ff20d5eb2bcf4182c8d14589b94
yssy3135 <ys3135@gmail.com>
2025-02-09 23:02:05 +0900

    articleView 구현

---
+++ service/view/src/main/java/ys/board/view/service/ArticleViewCountBackUpProcessor.java
@@ -0,0 +1,25 @@
+package ys.board.view.service;
+
+
+import ys.board.view.entity.ArticleViewCount;
+import ys.board.view.repository.ArticleViewCountBackUpRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleViewCountBackUpProcessor {
+    private final ArticleViewCountBackUpRepository articleViewCountBackUpRepository;
+
+    @Transactional
+    public void backUp(Long articleId, Long viewCount) {
+        int result = articleViewCountBackUpRepository.updateViewCount(articleId, viewCount);
+        if (result == 0) {
+            articleViewCountBackUpRepository.findById(articleId)
+                    .ifPresentOrElse(ignored -> { },
+                        () -> articleViewCountBackUpRepository.save(ArticleViewCount.init(articleId, viewCount))
+                    );
+        }
+    }
+}
---
+++ service/view/src/main/resources/db/rdb-schema.sql
@@ -0,0 +1,4 @@
+create table article_view_count (
+    article_id bigint not null primary key,
+    view_count bigint not null
+);
